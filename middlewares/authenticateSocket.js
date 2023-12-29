3909b9495b83ad649ae8bb61023fd63e05132594
thsergitox <sergio.pezo.j@uni.pe>
2023-12-28 19:14:23 -0500

    updt: "Security bug fixed"

--- middlewares/authenticateSocket.js
+++ middlewares/authenticateSocket.js
@@ -1,6 +1,6 @@
 export const authenticateSocket = (socket, next) => {
   const token = socket.handshake.query.token
-  if (token === process.env.API_KEY) {
+  if (token && token === process.env.API_KEY) {
     next()
   } else {
     next(new Error('Failed authentication'))
